d04bbe37ed4da51a35e1766df0c3066397e0f169
juanrabaro <jrbaro@iesrafaelalberti.es>
2024-01-29 10:51:12 +0100

    crear y borrar preguntas hecho

--- Parte_3/questions.sql
+++ Parte_3/questions.sql
@@ -6,32 +6,32 @@ VALUES ('one_choice', '¿Qué significa PHP?'),
        ('one_choice', '¿Qué función de PHP se utiliza para abrir un archivo para escritura?');
 
 INSERT INTO Options (correct_answer, option_type, answer_choice, Questions_question_id, option_text)
-VALUES (false, 'one_choice', 'a', 1, 'Página de inicio personal'),
-       (true, 'one_choice', 'b', 1, 'PHP: Procesador de hipertexto (Respuesta correcta)'),
-       (false, 'one_choice', 'c', 1, 'Procesador de hipervínculos privados'),
-       (false, 'one_choice', 'd', 1, 'Página de enlace PHP');
+VALUES (false, 'one_choice', 'a', 7, 'Página de inicio personal'),
+       (true, 'one_choice', 'b', 7, 'PHP: Procesador de hipertexto'),
+       (false, 'one_choice', 'c', 7, 'Procesador de hipervínculos privados'),
+       (false, 'one_choice', 'd', 7, 'Página de enlace PHP');
 
 INSERT INTO Options (correct_answer, option_type, answer_choice, Questions_question_id, option_text)
-VALUES (false, 'one_choice', 'a', 2, 'Entero'),
-       (false, 'one_choice', 'b', 2, 'Booleano'),
-       (true, 'one_choice', 'c', 2, 'Caracter'),
-       (false, 'one_choice', 'd', 2, 'Flotante');
+VALUES (false, 'one_choice', 'a', 8, 'Entero'),
+       (false, 'one_choice', 'b', 8, 'Booleano'),
+       (true, 'one_choice', 'c', 8, 'Caracter'),
+       (false, 'one_choice', 'd', 8, 'Flotante');
 
 INSERT INTO Options (correct_answer, option_type, answer_choice, Questions_question_id, option_text)
-VALUES (false, 'one_choice', 'a', 3, 'HelloWorld'),
-       (true, 'one_choice', 'b', 3, 'Hola Mundo'),
-       (false, 'one_choice', 'c', 3, 'HelloWorld'),
-       (false, 'one_choice', 'd', 3, '"Hola Mundo"');
+VALUES (false, 'one_choice', 'a', 9, 'HelloWorld'),
+       (true, 'one_choice', 'b', 9, 'Hola Mundo'),
+       (false, 'one_choice', 'c', 9, 'HelloWorld'),
+       (false, 'one_choice', 'd', 9, '"Hola Mundo"');
 
 INSERT INTO Options (correct_answer, option_type, answer_choice, Questions_question_id, option_text)
-VALUES (true, 'one_choice', 'a', 4, 'Bucle for'),
-       (false, 'one_choice', 'b', 4, 'Bucle while'),
-       (false, 'one_choice', 'c', 4, 'Bucle do...while'),
-       (false, 'one_choice', 'd', 4, 'Bucle foreach');
+VALUES (true, 'one_choice', 'a', 10, 'Bucle for'),
+       (false, 'one_choice', 'b', 10, 'Bucle while'),
+       (false, 'one_choice', 'c', 10, 'Bucle do...while'),
+       (false, 'one_choice', 'd', 10, 'Bucle foreach');
 
 INSERT INTO Options (correct_answer, option_type, answer_choice, Questions_question_id, option_text)
-VALUES (false, 'one_choice', 'a', 5, 'fopen'),
-       (false, 'one_choice', 'b', 5, 'file_open'),
-       (false, 'one_choice', 'c', 5, 'open_file'),
-       (true, 'one_choice', 'd', 5, 'Ninguna de las anteriores');
+VALUES (false, 'one_choice', 'a', 11, 'fopen'),
+       (false, 'one_choice', 'b', 11, 'file_open'),
+       (false, 'one_choice', 'c', 11, 'open_file'),
+       (true, 'one_choice', 'd', 11, 'Ninguna de las anteriores');
 
